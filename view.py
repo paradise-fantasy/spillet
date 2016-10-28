079c315b927ffce85a65935652fa61214a4f16c7
Haavard Fagervoll <fagervoll@gmail.com>
2016-10-28 23:03:48 +0200

    Ikkje braaaaa!

--- view.py
+++ view.py
@@ -9,15 +9,16 @@ from datetime import datetime
 from flask import render_template, request, session, jsonify
 
 app = Flask(__name__)
+app.debug = True
 
-
-# __file__ refers to the file settings.py 
+# __file__ refers to the file settings.py
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 200
+    render_template('index.html', title='Multiple Choice')
 
 if __name__ == '__main__':
     #HOST = environ.get('SERVER_HOST', 'localhost')
@@ -25,5 +26,5 @@ if __name__ == '__main__':
     try:
         PORT = int(environ.get('SERVER_PORT', '8000'))
     except ValueError:
-        PORT = 8000 
+        PORT = 8000
     app.run(HOST, PORT)
